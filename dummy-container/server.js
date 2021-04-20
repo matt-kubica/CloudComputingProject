5e36e8f10e8f9d20ce0773b4262c675f1c5169f9
qbic22562 <mateusz.kubica99@gmail.com>
2021-04-20 16:43:16 +0200

    Changed dummy container source code

--- dummy-container/server.js
+++ dummy-container/server.js
@@ -1,24 +1,15 @@
-const express = require('express')
-const axios = require('axios').default;
-const app = express()
-const port = 3000
+const app = require('express')()
+const os = require('os');
+const port = 80
+
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-  axios.get('http://tourism.opendatahub.bz.it/api/Location?language=en&type=null&showall=true&locfilter=null')
-  .then(function (response) {
-    // handle success
-    console.log(response.config.url);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  .then(function () {
-    console.log("server.js - app.get / executed")
+  res.send({
+    arch: os.arch(),
+    hostname: os.hostname(),
+    platform: os.platform(),
+    uptime: os.uptime(),
   });
-})
+});
 
-app.listen(port, () => {
-  console.log(`server.js listening at http://localhost:${port}`)
-})
+app.listen(port, () => console.log(`listening on port ${port}`));
